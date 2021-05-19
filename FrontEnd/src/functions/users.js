5cd5b72a2d2cf607e52aa3eb69c542483cc6defe
linharrush <linharrush@amil.com>
2021-05-19 15:30:22 +0300

    update

--- FrontEnd/src/functions/users.js
+++ FrontEnd/src/functions/users.js
@@ -12,16 +12,16 @@ export const  login = async (params) =>{
     return resp;
 }
 
-export const  checkemail = async (params) =>{
-    //option 2 for async/await handling
-    const resp = await axios.post('http://localhost:3001/checkemail', params);
-    console.log(resp);
-    if(resp == 200)
-    {
-        return true;
-    }
-    return false;
-}
+// export const  checkemail = async (params) =>{
+//     //option 2 for async/await handling
+//     const resp = await axios.post('http://localhost:3001/checkemail', params);
+//     console.log(resp);
+//     if(resp == 200)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
 
 export const  createUser = async (params) =>{
     //option 2 for async/await handling
